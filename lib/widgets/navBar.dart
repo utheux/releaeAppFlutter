531268a6d9cfa54f822145c2dae5df649411f89f
utheux <utheuz_ribeiro@hotmail.com>
2026-06-11 20:38:08 -0300

    feat: safeArea navBar

--- lib/widgets/navBar.dart
+++ lib/widgets/navBar.dart
@@ -9,65 +9,71 @@ class NavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        IconButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => NewsPage()),
-            );
-          },
-          icon: Icon(Icons.article_outlined),
-          iconSize: 35,
-          splashRadius: 24,
-          color: Color(0xFF2F2F2F),
-        ),
-        IconButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => CardsPage()),
-            );
-          },
-          icon: Icon(Icons.view_list_outlined),
-          iconSize: 35,
-          splashRadius: 24,
-          color: Color(0xFF2F2F2F),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.add_box_outlined),
-          iconSize: 35,
-          splashRadius: 24,
-          color: Color(0xFF2F2F2F),
-        ),
-        IconButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const FavoritesPage()),
-            );
-          },
-          icon: Icon(Icons.favorite_outline),
-          iconSize: 35,
-          splashRadius: 24,
-          color: Color(0xFF2F2F2F),
-        ),
-        IconButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ReviewCardsPage()),
-            );
-          },
-          icon: const Icon(Icons.rate_review_outlined),
-          iconSize: 35,
-          splashRadius: 24,
-          color: const Color(0xFF2F2F2F),
-        ),
-      ],
+    return SafeArea(
+      top: false,
+      minimum: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => NewsPage()),
+              );
+            },
+            icon: Icon(Icons.article_outlined),
+            iconSize: 35,
+            splashRadius: 24,
+            color: Color(0xFF2F2F2F),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CardsPage()),
+              );
+            },
+            icon: Icon(Icons.view_list_outlined),
+            iconSize: 35,
+            splashRadius: 24,
+            color: Color(0xFF2F2F2F),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add_box_outlined),
+            iconSize: 35,
+            splashRadius: 24,
+            color: Color(0xFF2F2F2F),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const FavoritesPage()),
+              );
+            },
+            icon: Icon(Icons.favorite_outline),
+            iconSize: 35,
+            splashRadius: 24,
+            color: Color(0xFF2F2F2F),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ReviewCardsPage(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.rate_review_outlined),
+            iconSize: 35,
+            splashRadius: 24,
+            color: const Color(0xFF2F2F2F),
+          ),
+        ],
+      ),
     );
   }
 }
